fbca8732aa7e1d6c8dd2f89dc3a42a46c5020b3b
blrkartick <kalyan3337@gmail.com>
2025-09-09 11:53:35 +0000

    added provider new

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "6.4.0"
     }
   }
-  backend "remote" {
-      organisation = "kalyanpath-demo"
-      workspaces {
-        name = "github-actions"
-}
-}
-}
 provider "aws" {
   region = "us-east-1"
 }

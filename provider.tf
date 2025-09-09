8bec2d2aceb5fca9d6b67de02eafb602d5b77541
blrkartick <kalyan3337@gmail.com>
2025-09-09 12:44:34 +0000

    added provider new13

--- provider.tf
+++ provider.tf
@@ -4,9 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.4.0"
     }
-  }
 }
-
+cloud { 
+    organization = "kalyanpath-demo" 
+    workspaces { 
+      name = "github-actions" 
+    } 
+  } 
+}
 provider "aws" {
   region = "us-east-1"
 }

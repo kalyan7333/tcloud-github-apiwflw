0c948b5f95cb1c6e9bf289476ff69f8295aefc3f
blrkartick <kalyan3337@gmail.com>
2025-09-09 12:21:19 +0000

    added provider new8

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
+# Terraform AWS provider
 terraform {
+  required_version = ">= 1.0.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.4.0"
+      version = "~> 3.0"
     }
   }
-} 
+}
 provider "aws" {
     region = "us-east-1"
-  }
-
+    profile = "default"
+}
